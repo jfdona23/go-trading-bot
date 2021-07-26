73bfcd464b2387b6a02deb86cd7620874617872f
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-07-26 13:06:31 -0300

    Minor fix in function name

--- helpers.go
+++ helpers.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 // Get an environment value or returns a fallback value if not present
-func Getenv(environmentVar string, defaultValue string) string {
+func getenv(environmentVar string, defaultValue string) string {
 	value, exists := os.LookupEnv(environmentVar)
 	if !exists {
 		return defaultValue
